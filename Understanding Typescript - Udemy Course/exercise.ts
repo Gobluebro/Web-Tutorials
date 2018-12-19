f7157efd773684d7703fea5482a6d04710b688c8
Griffin Strayer <griffinstrayer@gmail.com>
2018-12-18 22:02:02 -0500

    Adding exercise and correct index

---
+++ Understanding Typescript - Udemy Course/exercise.ts
@@ -0,0 +1,18 @@
+//Try to be as explicit as possible and add Types to everything you can!
+
+let bankAccount = {
+  money: 2000,
+  deposit(value) {
+    this.money += value;
+  }
+};
+
+let myself = {
+  name: "Max",
+  bankAccount: bankAccount,
+  hobbies: ["Sports", "Cooking"]
+};
+
+myself.bankAccount.deposit(3000);
+
+console.log(myself);
