a4d792a833f3a65a8221de8def25422d3787280c
Griffin Strayer <griffinstrayer@gmail.com>
2018-12-18 22:11:29 -0500

    My attempt at the exercise for being explicit

--- Understanding Typescript - Udemy Course/exercise.ts
+++ Understanding Typescript - Udemy Course/exercise.ts
@@ -1,13 +1,13 @@
 //Try to be as explicit as possible and add Types to everything you can!
 
-let bankAccount = {
+let bankAccount: { money: number; deposit: (value: number) => void } = {
   money: 2000,
   deposit(value) {
     this.money += value;
   }
 };
 
-let myself = {
+let myself: { name: string; bankAccount: any; hobbies: string[] } = {
   name: "Max",
   bankAccount: bankAccount,
   hobbies: ["Sports", "Cooking"]
