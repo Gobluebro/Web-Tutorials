23931645dead08f78d2db99107d6193d35b2fd74
Griffin Strayer <griffinstrayer@gmail.com>
2019-06-11 16:15:36 -0400

    Scoped Styles
    Only use styles for each specific html set of elements

--- Vue JS 2 - The Complete Guide(incl. Vue Router & Vuex)/vue-cli/src/components/Server/ServerDetails.vue
+++ Vue JS 2 - The Complete Guide(incl. Vue Router & Vuex)/vue-cli/src/components/Server/ServerDetails.vue
@@ -1,4 +1,4 @@
-  <template>
+<template>
   <div class="col-xs-12 col-sm-6">
     <p>Server Details are currently not updated</p>
   </div>
@@ -6,4 +6,11 @@
 
 <script>
 </script>
+
+<style scoped>
+div {
+  border: 1px solid red;
+}
+</style>
+
 
