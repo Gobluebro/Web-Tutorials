16a5c0234e80a417fd5e0fb9baa18692cc22d3ad
Griffin Strayer <griffinstrayer@gmail.com>
2019-06-11 10:45:49 -0400

    Assignment 6 Completed

---
+++ Vue JS 2 - The Complete Guide(incl. Vue Router & Vuex)/vue-cli/Assignment 6/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="container">
+    <app-header></app-header>
+    <hr>
+    <app-server-details></app-server-details>
+    <hr>
+    <app-footer></app-footer>
+  </div>
+</template>
+
+<script>
+import Header from "./Header.vue";
+import ServerDetails from "./ServerDetails.vue";
+import Footer from "/Footer.vue";
+
+export default {
+  components: {
+    "app-header": Header,
+    "app-server-details": ServerDetails,
+    "app-footer": Footer
+  }
+};
+</script>
+
+<style>
+</style>
