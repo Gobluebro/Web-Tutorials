50c2e12570305f1f3be0b0a4fef32d399589118e
Griffin Strayer <griffinstrayer@gmail.com>
2019-06-12 10:40:36 -0400

    Custom Events for child to parent communication

--- Vue JS 2 - The Complete Guide(incl. Vue Router & Vuex)/vue-cli/src/components/UserDetail.vue
+++ Vue JS 2 - The Complete Guide(incl. Vue Router & Vuex)/vue-cli/src/components/UserDetail.vue
@@ -3,6 +3,7 @@
     <h3>You may view the User Details here</h3>
     <p>Many Details</p>
     <p>User Name: {{switchName()}}</p>
+    <button @click="resetName()">Reset Name</button>
   </div>
 </template>
 
@@ -21,6 +22,11 @@ export default {
         .split("")
         .reverse()
         .join("");
+    },
+    resetName() {
+      this.myName = "Griffin";
+      // this allows us to emit a custom event, so we can pass name to the parent
+      this.$emit("nameWasReset", this.myName);
     }
   }
 };
