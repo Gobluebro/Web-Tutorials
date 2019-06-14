bc218ad8a4e4b14dbe263d58b8352a92380ddc20
Griffin Strayer <griffinstrayer@gmail.com>
2019-06-14 12:11:27 -0400

    Assignment 8

--- Vue JS 2 - The Complete Guide(incl. Vue Router & Vuex)/vue-cli/src/App.vue
+++ Vue JS 2 - The Complete Guide(incl. Vue Router & Vuex)/vue-cli/src/App.vue
@@ -1,49 +1,47 @@
 <template>
-  <div class="container">
-    <div class="row">
-      <div class="col-xs-12">
-        <button @click="selectedComponent= 'appQuote'">Quote</button>
-        <button @click="selectedComponent= 'appAuthor'">Author</button>
-        <button @click="selectedComponent= 'appNew'">New</button>
-        <hr>
-        <p>{{selectedComponent}}</p>
-        <keep-alive>
-          <component :is="selectedComponent">
-            <p>Default Content</p>
-          </component>
-        </keep-alive>
-        <!-- <app-quote quote="'A wonderful quote!''">
-          <h2 slot="title">{{quoteTitle}}</h2>
-          <p>A wonderful Quote</p>
-        </app-quote>-->
-      </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12">
+                <br>
+                <button class="btn btn-primary" @click="selectedComponent = 'appBlue'">Load Blue Template</button>
+                <button class="btn btn-success" @click="selectedComponent = 'appGreen'">Load Green Template</button>
+                <button class="btn btn-danger" @click="selectedComponent = 'appRed'">Load Red Template</button>
+                <hr>
+                <component :is="selectedComponent">
+                    <p>This is the Content</p>
+                </component>
+                <!--<app-blue>-->
+                    <!--<p>This is the Content</p>-->
+                <!--</app-blue>-->
+                <!--<app-green>-->
+                    <!--<p>This is the Content</p>-->
+                <!--</app-green>-->
+                <!--<app-red>-->
+                    <!--<p>This is the Content</p>-->
+                <!--</app-red>-->
+            </div>
+        </div>
     </div>
-  </div>
 </template>
 
 <script>
-import Quote from "./components/Quote";
-import Author from "./components/Author";
-import New from "./components/New";
+    import Blue from './components/Blue.vue';
+    import Green from './components/Green.vue';
+    import Red from './components/Red.vue';
 
-export default {
-  data: function() {
-    return {
-      quoteTitle: "The Quote",
-      selectedComponent: "appQuote"
-    };
-  },
-  components: {
-    appQuote: Quote,
-    appAuthor: Author,
-    appNew: New
-  }
-};
+    export default {
+        data: function() {
+          return {
+              selectedComponent: 'appBlue'
+          }
+        },
+        components: {
+            appBlue: Blue,
+            appGreen: Green,
+            appRed: Red
+        }
+    }
 </script>
 
-<style scoped>
-/* doesn't pass in */
-/* h2 {
-  color: red;
-} */
+<style>
 </style>
