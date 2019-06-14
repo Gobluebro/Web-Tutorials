af7efebf09f100ee1b845ec76f1f604bdec9bc75
Griffin Strayer <griffinstrayer@gmail.com>
2019-06-14 10:20:19 -0400

    Assignment completed solution

--- Vue JS 2 - The Complete Guide(incl. Vue Router & Vuex)/vue-cli/src/components/Server/Servers.vue
+++ Vue JS 2 - The Complete Guide(incl. Vue Router & Vuex)/vue-cli/src/components/Server/Servers.vue
@@ -1,12 +1,29 @@
 <template>
   <div class="col-xs-12 col-sm-6">
     <ul class="list-group">
-      <li class="list-group-item" v-for="(index, key) in 5" :key="key">Server #{{ index }}</li>
+      <app-server v-for="(server, key) in servers" :server="server" :key="key"></app-server>
     </ul>
   </div>
 </template>
 
 <script>
+import Server from "./Server.vue";
+
+export default {
+  data: function() {
+    return {
+      servers: [
+        { id: 1, status: "Normal" },
+        { id: 2, status: "Critical" },
+        { id: 3, status: "Unknown" },
+        { id: 4, status: "Normal" }
+      ]
+    };
+  },
+  components: {
+    appServer: Server
+  }
+};
 </script>
 
 <style>
